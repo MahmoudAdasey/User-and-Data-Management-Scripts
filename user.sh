b9a4f649bb94f16a4f234dbe1ea0e6069bc0e281
MahmoudAdasey <m.ad42@yahoo.com>
2025-01-10 22:18:16 +0200

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+CSV_FILE="employee.csv"
+
+if [ "$UID" -ne 0 ]
+then
+        echo "this script must be run as root."
+        exit 1
+fi
+
+if [ ! -f "$CSV_FILE" ]
+then
+        echo "CSV file not found: $CSV_FILE"
+        exit 1
+fi
+
+while IFS=, read -r username full_name
+do
+        password=$(openssl rand -base64 12)
+
+        useradd -m -c "$full_name" "$username"
+        echo "$username:$password" | chpasswd
+
+        chage -d 0 "$username"
+
+        echo "user created: $username"
+        echo "Full Name: $full_name"
+        echo "Random Password: $password"
+        echo "---------------------------"
+
+        echo " $username,$password" >> out.txt
+
+done < $CSV_FILE
+
+echo "User Creation completed."
